3f3991513c9a62fe36f35d5b37a4350a0ad8ab80
jonik2909 <boronovjonibek@gmail.com>
2026-03-16 08:31:38 +0900

    feat: Java Variables and Data Types

---
+++ 01. variables/Variables.java
@@ -0,0 +1,39 @@
+public class Variables {
+    public static void main(String[] args) {
+        // int age = 25;
+        // System.out.println("before " + age);
+
+        // age = 30;
+        // System.out.println("after " + age);
+
+        // BIT 0 | 1
+        // A => 01000001
+        // BYTE => 1 BYTE (01000001)
+
+        System.out.println("====== DATA TYPES ======");
+
+        byte temperature = 21;
+        System.out.println("temperature: " + temperature);
+
+        int age = 25;
+        System.out.println("age " + age);
+
+        long population = 8000000000L;
+        System.out.println("population " + population);
+
+        double price = 19.991232;
+        System.out.println("price " + price);
+
+        float price_float = 19.9f;
+        System.out.println("price_float " + price_float);
+
+        char grade = 'A';
+        System.out.println("grade " + grade);
+
+        boolean isAdmin = false;
+        System.out.println("isAdmin " + isAdmin);
+
+        String text = "Hello Java!";
+        System.out.println("text: " + text);
+    }
+}
